54927faeadc0bf66ea16d03e91119b81dcde8078
Danilo osorio <daosorio@capiro.co>
2024-04-30 16:38:26 -0500

    two buttons

--- image-preview/src/main/java/com/plcoding/image_preview/ImagePreview.kt
+++ image-preview/src/main/java/com/plcoding/image_preview/ImagePreview.kt
@@ -2,24 +2,11 @@ package com.plcoding.image_preview
 
 import android.content.Context
 import android.widget.Toast
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
     @Composable
@@ -34,3 +21,15 @@ import androidx.compose.ui.unit.dp
             Text(text = "Click to button")
         }
     }
+@Composable
+fun ButtonDanilo(context: Context){
+    Button(onClick = {
+        Toast.makeText(context, "Teo Chupa Monda", Toast.LENGTH_SHORT).show()
+    },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Text(text = "Click to button")
+    }
+}
