40a1006ec5a40e8d973590f713112d5a4b59f003
Danilo osorio <daosorio@capiro.co>
2024-04-30 14:45:46 -0500

    asaf

--- image-preview/src/main/java/com/plcoding/image_preview/ImagePreview.kt
+++ image-preview/src/main/java/com/plcoding/image_preview/ImagePreview.kt
@@ -1,10 +1,13 @@
 package com.plcoding.image_preview
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -19,46 +22,15 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
-@Composable
-fun ImagePreview(
-    image: Painter,
-    modifier: Modifier = Modifier,
-    description: String = "",
-    contentDescription: String = "",
-    onImageClick: () -> Unit = {}
-) {
-    Box(
-        modifier = modifier
-            .aspectRatio(1f)
-            .clip(RoundedCornerShape(10.dp))
-            .shadow(15.dp, RoundedCornerShape(15.dp))
-            .clickable { onImageClick() }
-    ) {
-        Image(
-            painter = image,
-            contentDescription = contentDescription,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .fillMaxSize()
-        )
-        Text(
-            text = description,
-            style = MaterialTheme.typography.body1,
-            color = Color.White,
-            overflow = TextOverflow.Ellipsis,
-            maxLines = 1,
+    @Composable
+    fun ButtonTeo(context: Context){
+        Button(onClick = {
+            Toast.makeText(context, "Teo Chupa Monda", Toast.LENGTH_SHORT).show()
+        },
             modifier = Modifier
                 .fillMaxWidth()
-                .background(
-                    Brush.verticalGradient(
-                        listOf(
-                            Color.Transparent,
-                            Color.Black
-                        )
-                    )
-                )
-                .align(Alignment.BottomStart)
-                .padding(8.dp)
-        )
+                .padding(16.dp)
+        ) {
+            Text(text = "Click me")
+        }
     }
-}
